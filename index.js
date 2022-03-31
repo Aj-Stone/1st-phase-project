a72881a6ce519c41e5cf01b92eee062d91323a7e
Alex Atsitty <atsitty.alex1128@gmail.com>
2022-03-31 10:56:53 -0600

    Pictures from server

---
+++ index.js
@@ -0,0 +1,20 @@
+function appendChord(chord){
+  let list = document.createElement("li")
+  list.className = "strums"
+  list.innerHTML = `
+  <h2>${chord.name}</h2>
+  <img src = "${chord.image}">
+  <div class = "button">
+    <button> Listen </button>
+  </div>
+  `
+  document.querySelector('#chord-content').appendChild(list)
+}
+
+function getChords(){
+  fetch('http://localhost:3000/chords')
+  .then(res=>res.json())
+  .then(chords => chords.forEach(chord => appendChord(chord)))
+}
+
+getChords()
