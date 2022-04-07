86891e707cee3576381f8eebbaa79bcd988568d2
Alex Atsitty <atsitty.alex1128@gmail.com>
2022-04-07 12:22:27 -0600

    Sounds added

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 function appendChord(chord){
-  let list = document.createElement("ul")
-  list.className = "strums"
+  let list = document.createElement("li")
   list.innerHTML = `
   <div class="insert">
     <h2>${chord.name}</h2>
     <img src = "${chord.image}">
     <div class = "button">
-      <button> Listen </button>
+      <button id="${chord.idName}"> Listen </button>
     </div>
   </div>
   `
   document.querySelector('#chord-content').appendChild(list)
 }
 
-// document.addEventListener('click', () => {
-//   event.target.
-// })
+document.addEventListener('click', () => {
+  if(event.target === document.querySelector('#g-chord')){
+    const gAud = new Audio("C:/Users/AJ/OneDrive/Documents/sounds/Chord-G.m4a")
+    gAud.play()
+  }
+  else if(event.target === document.querySelector('#c-chord')){
+    const cAud = new Audio("C:/Users/AJ/OneDrive/Documents/sounds/Chord-C.m4a")
+    cAud.play()
+  }
+  else if(event.target === document.querySelector('#d-chord')){
+    const dAud = new Audio("C:/Users/AJ/OneDrive/Documents/sounds/Chord-D.m4a")
+    dAud.volume = 0.5
+    dAud.play()
+  }
+  else if(event.target === document.querySelector('#e-chord')){
+    const eAud = new Audio("C:/Users/AJ/OneDrive/Documents/sounds/Chord-Em.m4a")
+    eAud.volume = 0.4
+    eAud.play()
+  }
+})
 
 function getChords(){
   fetch('http://localhost:3000/chords')
